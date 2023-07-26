dd8cf4cb94f055c48ad421f9c31df2f438c71cd6
Zhi Yang Chen <zhiyangchen003@gmail.com>
2023-07-26 14:04:23 -0400

    connected database using mongoose

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp');
+
+// Connecting to a database
+const db = mongoose.connection;
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once("open", () => {
+    console.log("Database connected");
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Directories
 app.get('/', (req, res) => {
-    //res.send("hello from yelpcamp");
     res.render('home');
-})
+});
 
 app.listen(3000, ()=> {
     console.log("serving on port 3000");
-})
+});
