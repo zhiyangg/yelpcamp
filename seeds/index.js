7d74111dcb05b5f36dfe7be4c8cf4c99b299fdc0
Zhi Yang Chen <zhiyangchen003@gmail.com>
2023-07-28 18:14:09 -0400

    added Bootstrap, Boilerplate, navbar, footer, and images

--- seeds/index.js
+++ seeds/index.js
@@ -19,10 +19,16 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async() => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const rand1000 = Math.floor(Math.random() * 1000); // random number 1-1000
+        
+        const rand1000 = Math.floor(Math.random() * 1000); 
+        const price = Math.floor(Math.random() * 20) + 10;
+
         const camp = new Campground({
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`, // city, state
-            title: `${sample(descriptors)} ${sample(places)}` // descriptor, places
+            title: `${sample(descriptors)} ${sample(places)}`, // descriptor, places
+            image: 'https://images.unsplash.com/photo-1461353789837-8eb180f968d2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODQzNTF8fHx8fHx8MTY5MDU4MTgxMA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+            description: 'lorem ipsum',
+            price
         })
         await camp.save();
     }
