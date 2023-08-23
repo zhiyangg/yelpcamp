6c8e6b330495db3db22a4097938aecd543c3b78c
Zhi Yang Chen <zhiyangchen003@gmail.com>
2023-08-22 21:27:45 -0400

    completed section53, added controllers folder and star ratings

--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,21 @@
+// Imports
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utilities/catchAsync');
-const User = require('../models/user');
+const users = require('../controllers/users');
 
 const {storeReturnTo} = require('../middleware');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-})
-router.post('/register', catchAsync(async(req, res) => {
-    try {
-        const {email, username, password} = req.body;
-        const user = new User({email, username});
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        });
-    } catch(e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}));
+// Routes
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/login', (req, res) => {
-    res.render('users/login');
-});
-router.post('/login', storeReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
-    req.flash('success', 'Welcome Back!');
-    const redirectUrl = res.locals.returnTo || 'campgrounds';
-    res.redirect(redirectUrl);
-});
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login);
 
-router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-        if (err) return next(err);
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-});
+router.get('/logout', users.logout);
 
 module.exports = router;
