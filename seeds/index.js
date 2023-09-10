29a6894bddeb41e68a86a8e6b7b4c4727022e231
Zhi Yang Chen <zhiyangchen003@gmail.com>
2023-09-10 02:11:34 -0400

    tweaked show page and images

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const Campground = require('../models/campground');
 
-const {places, descriptors} = require('./seedHelpers')
+const {places, descriptors, shuffledImages} = require('./seedHelpers')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp');
 
@@ -16,27 +16,32 @@ mongoose.connection.once("open", () => {
 // function for picking a random place and random descriptor
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+// function to shuffle an array using Fisher-Yates shuffle algorithm
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 const seedDB = async() => {
     await Campground.deleteMany({});
     for (let i = 0; i < 200; i++) {
         const rand1000 = Math.floor(Math.random() * 1000); 
         const price = Math.floor(Math.random() * 20) + 10;
+        
         const camp = new Campground({
             author: '64e8301bf581a72b5cfbd538',
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`, // city, state
             title: `${sample(descriptors)} ${sample(places)}`, // descriptor, places
-            description: 'lorem ipsum',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
             geometry: {
                 type: "Point",
                 coordinates: [cities[rand1000].longitude, cities[rand1000].latitude]
             },
-            images: [
-                {
-                  url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1693531550/YelpCamp/vkynirpzria02jqclaw3.jpg',
-                  filename: 'YelpCamp/vkynirpzria02jqclaw3',
-                }
-            ]
+            images: shuffleArray(shuffledImages)
         })
         await camp.save();
     }
--- seeds/seedHelpers.js
+++ seeds/seedHelpers.js
@@ -43,3 +43,41 @@ module.exports.places = [
     'Hollow'
 ]
 
+module.exports.shuffledImages = [
+    {
+      url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1694325534/YelpCamp/w0h3jz52lok3d3runt9v.jpg',
+      filename: 'YelpCamp/w0h3jz52lok3d3runt9v',
+    },
+    {
+      url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1694325533/YelpCamp/g7sjotq8anfxny6iov67.jpg',
+      filename: 'YelpCamp/g7sjotq8anfxny6iov67',
+    },
+    {
+      url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1694325533/YelpCamp/bhe1submcv8l62pnjpje.jpg',
+      filename: 'YelpCamp/bhe1submcv8l62pnjpje',
+    },
+    {
+      url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1694326136/YelpCamp/sfeoenbe5x5u81tblyup.jpg',
+      filename: 'YelpCamp/sfeoenbe5x5u81tblyup',
+    },
+    {
+      url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1694326136/YelpCamp/rr28fdtseboufdfwzz2x.jpg',
+      filename: 'YelpCamp/rr28fdtseboufdfwzz2x',
+    },
+    {
+      url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1694326137/YelpCamp/aax8ujiwatpopjipwtdp.jpg',
+      filename: 'YelpCamp/aax8ujiwatpopjipwtdp',
+    },
+    {
+      url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1694326137/YelpCamp/iatxvqzndclvya7bwhmf.jpg',
+      filename: 'YelpCamp/iatxvqzndclvya7bwhmf',
+    },
+    {
+      url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1694326137/YelpCamp/etwf9k5ehpott8r3end4.jpg',
+      filename: 'YelpCamp/etwf9k5ehpott8r3end4',
+    },
+    {
+      url: 'https://res.cloudinary.com/de6e0wvfl/image/upload/v1694326138/YelpCamp/mzhyfhvklmuxzrocrntt.jpg',
+      filename: 'YelpCamp/mzhyfhvklmuxzrocrntt',
+    }
+  ]
