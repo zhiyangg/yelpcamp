398c7684899436a63be9bd8849bc5b769ca2e9aa
Zhi Yang Chen <zhiyangchen003@gmail.com>
2023-09-10 16:03:21 -0400

    fixing app.js port for deployment

--- app.js
+++ app.js
@@ -151,6 +151,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err});
 })
 
-app.listen(3000, ()=> {
-    console.log("serving on port 3000");
+app.listen(80, ()=> {
+    console.log("serving on port 80");
 });
